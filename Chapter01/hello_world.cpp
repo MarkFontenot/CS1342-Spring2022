e3064380891c4d756498bfcbdb3354c797f62e2e
Erik Gabrielsen <erik.gabrielsen@dialexa.com>
2022-01-13 11:26:49 -0600

    setting up repo

---
+++ Chapter01/hello_world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>  // cout, cin
+#include <string>
+
+using namespace std;
+
+int main() {
+  cout << "Insert a value: " << endl;
+  double value;
+  cin >> value;  // 10
+
+  cout << "The value is storing: " << value << endl;
+  return 0;  // success code
+}
---
+++ Chapter02/random.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>  // rand & srand
+#include <ctime>    // time
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  srand(time(NULL));
+  int random{rand() % 100};
+
+  cout << "Random Number: " << random << endl;
+
+  return 0;
+}
